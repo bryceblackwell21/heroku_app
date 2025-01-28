f27d077f62e301044aeccca7e608d7df8e4b7850
bryceblackwell21 <bryceblackwell21@gmail.com>
2025-01-28 11:02:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
-# Initialize the Flask app
 app = Flask(__name__)
 
-# Define the route for the home page
 @app.route("/")
 def home():
-    return "Hello, World! This app is running on Heroku."
+    return render_template("index.html")
 
-# Run the app
 if __name__ == "__main__":
-    # Heroku dynamically assigns the port, so use that if available
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
